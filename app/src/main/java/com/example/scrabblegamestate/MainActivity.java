1126b1fba7d196b29756af124d108e43af8db1a3
hilerio23 <hilerio23@up.edu>
2021-03-11 14:11:18 -0800

    I got the toString method to work and also the onClick is now working

--- app/src/main/java/com/example/scrabblegamestate/MainActivity.java
+++ app/src/main/java/com/example/scrabblegamestate/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.scrabblegamestate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button runTest = (Button)findViewById(R.id.button);
+        EditText info = (EditText)findViewById(R.id.editTextTextMultiLine);
+
+        runTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.getText().clear();
+
+                ScrabbleState firstInstance = new ScrabbleState();
+                ScrabbleState secondInstance = new ScrabbleState(firstInstance);
+                firstInstance.playWord(firstInstance);
+                info.append("The player is writing down the word frog\n");
+
+                firstInstance.isLegal(firstInstance);
+                info.append("The move the player made was legal\n");
+
+                firstInstance.exchange(firstInstance);
+                info.append("The player did a letter exchange\n");
+
+                firstInstance.pass(firstInstance);
+                info.append("The player passed on their turn\n");
+
+                ScrabbleState thirdInstance = new ScrabbleState();
+                ScrabbleState fourthInstance = new ScrabbleState(thirdInstance);
+
+                info.append(secondInstance.toString());
+                info.append(fourthInstance.toString());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/scrabblegamestate/ScrabbleState.java
@@ -0,0 +1,144 @@
+package com.example.scrabblegamestate;
+
+import java.util.Random;
+
+public class ScrabbleState {
+
+        //15 x 15 board
+        private ScrabbleLetter[][] board = new ScrabbleLetter[15][15];
+
+        //an in to tell whos move it is 0 for human 1 for AI
+        private int playerToMove;
+
+        //array of SrabbleLetters for hand
+        private ScrabbleLetter[] player1Hand = new ScrabbleLetter[7];
+        private ScrabbleLetter[] player2Hand = new ScrabbleLetter[7];
+
+        //array of ScrabbleLetters for Pool (100) total letters
+        private ScrabbleLetter[] pool = new ScrabbleLetter[100];
+
+        //game pause: 1 for pause 0 for playing
+        private int gamePause;
+
+
+        //constructor
+        public ScrabbleState(){
+
+            //sets board to blanks
+            for(int i = 0; i < 15; i++) {
+                for (int j = 0; j < 15; j++) {
+                    board[i][j] = new ScrabbleLetter(' ');
+                }
+            }
+
+            for(int i = 0; i < 7; i++){
+                //generating random chars for each player
+                Random rnd = new Random();
+                char randomChar1 = (char) ('a' + rnd.nextInt(26));
+                char randomChar2 = (char) ('a' + rnd.nextInt(26));
+                //creating SrabbleLetters
+                player1Hand[i] = new ScrabbleLetter(randomChar1);
+                player2Hand[i] = new ScrabbleLetter(randomChar2);
+            }
+
+            for(int i = 0; i < 100; i++){
+                Random rnd = new Random();
+                char randomChar = (char) ('a' + rnd.nextInt(26));
+                //doing random pool for now we can change how many of each letters we want later
+                pool[i] = new ScrabbleLetter(randomChar);
+            }
+
+            //player 0 is human player
+            playerToMove = 0;
+            //0 = playing, 1 = pause
+            gamePause = 0;
+
+        }
+
+        //deep copy
+        public ScrabbleState(ScrabbleState scrabbleStateCopy){
+            this.playerToMove = scrabbleStateCopy.playerToMove;
+            this.gamePause = scrabbleStateCopy.gamePause;
+            this.pool = scrabbleStateCopy.pool;
+            this.board = scrabbleStateCopy.board;
+            this.player1Hand = scrabbleStateCopy.player1Hand;
+            this.player2Hand = null; //player1 can't see player 2 hand
+        }
+
+        @Override
+        public String toString(){
+            String output;
+            output = "\ngame pause: " + this.gamePause + "\n";
+            output += "player to move: " + this.playerToMove + "\n";
+
+            output += "board: The board is empty";
+            for(int i = 0; i < 15; i++) {
+                for (int j = 0; j < 15; j++) {
+                    output += this.board[i][j].getLetter() + " ";
+                }
+            }
+            output += "\n";
+
+            output += "pool: \n";
+            for(int i = 0; i < 100; i++){
+                output += this.pool[i].getLetter() + " ";
+            }
+            output += "\n";
+
+            output += "player one hand: \n";
+            for(int i = 0; i < 7; i++){
+                output += this.player1Hand[i].getLetter() + " ";
+            }
+            output += "\n";
+
+            if(this.player2Hand != null) {
+                output += "player two hand: \n";
+                for (int i = 0; i < 7; i++) {
+                    output += this.player2Hand[i].getLetter() + " ";
+                }
+            }
+            output += "\n";
+            return output;
+
+        }
+
+        /**
+         *
+         * Methods in actions.txt
+         *Also got rid of challenge because I don't think it's necessary
+         *
+         */
+
+        public boolean isLegal(ScrabbleState scrabbleState){
+            //code to determine if action is legal
+            return true;
+        }
+
+        public boolean playWord(ScrabbleState scrabbleState){
+            if(isLegal(scrabbleState) == true){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public boolean pass(ScrabbleState scrabbleState){
+            if(isLegal(scrabbleState) == true){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public boolean exchange(ScrabbleState scrabbleState){
+            if(isLegal(scrabbleState) == true){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+}
