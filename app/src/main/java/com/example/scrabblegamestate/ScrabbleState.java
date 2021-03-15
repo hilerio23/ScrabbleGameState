a44860803a4ebda18f6be190cd65c1bbdd10214f
samonew <59709645+samonew@users.noreply.github.com>
2021-03-14 17:31:27 -0700

    Implemented Tribelhorn's notes

--- app/src/main/java/com/example/scrabblegamestate/ScrabbleState.java
+++ app/src/main/java/com/example/scrabblegamestate/ScrabbleState.java
@@ -19,7 +19,8 @@ public class ScrabbleState {
         //array of SrabbleLetters for hand
         private ScrabbleLetter[] player1Hand = new ScrabbleLetter[7];
         private ScrabbleLetter[] player2Hand = new ScrabbleLetter[7];
-        //add players 3 and 4
+        private ScrabbleLetter[] player3Hand = new ScrabbleLetter[7];
+        private ScrabbleLetter[] player4Hand = new ScrabbleLetter[7];
 
         //array of ScrabbleLetters for Pool (100) total letters
         private ScrabbleLetter[] pool = new ScrabbleLetter[100]; //arraylist
@@ -28,10 +29,16 @@ public class ScrabbleState {
         private int gamePause;
 
         //add score
+        private int playerOneScore = 0;
+        private int playerTwoScore = 0;
+        private int playerThreeScore = 0;
+        private int playerFourScore = 0;
 
-        //track how full the board is
+        //track how empty the percent is
+        double percentOfPool = 0.0;
 
         //extra variable for tracking if a move is possible
+        boolean isPossible;
 
         //constructor
         public ScrabbleState(){
@@ -48,10 +55,15 @@ public class ScrabbleState {
                 Random rnd = new Random();
                 char randomChar1 = (char) ('a' + rnd.nextInt(26));
                 char randomChar2 = (char) ('a' + rnd.nextInt(26));
+                char randomChar3 = (char) ('a' + rnd.nextInt(26));
+                char randomChar4 = (char) ('a' + rnd.nextInt(26));
 
                 //creating ScrabbleLetters
                 player1Hand[i] = new ScrabbleLetter(randomChar1);
                 player2Hand[i] = new ScrabbleLetter(randomChar2);
+                player3Hand[i] = new ScrabbleLetter(randomChar3);
+                player4Hand[i] = new ScrabbleLetter(randomChar4);
+
             }
 
             //creating a random pool of all of the letter tiles
@@ -69,14 +81,19 @@ public class ScrabbleState {
         }
 
         //Deep copy of the given Scrabble State
-        //need to copy all the places in the board
         public ScrabbleState(ScrabbleState scrabbleStateCopy){
             this.playerToMove = scrabbleStateCopy.playerToMove;
             this.gamePause = scrabbleStateCopy.gamePause;
             this.pool = scrabbleStateCopy.pool;
-            this.board = scrabbleStateCopy.board;
+            for(int x = 0; x < board.length; x++){
+                for(int y = 0; y < board[x].length; y++){
+                    board[x][y] = scrabbleStateCopy.board[x][y];
+                }
+            }
             this.player1Hand = scrabbleStateCopy.player1Hand;
             this.player2Hand = null; //player1 can't see player 2 hand
+            this.player3Hand = null;
+            this.player4Hand = null;
         }
 
         @Override
@@ -117,6 +134,22 @@ public class ScrabbleState {
                 output += "player two hand: \n";
                 for (int i = 0; i < 7; i++) {
                     output += this.player2Hand[i].getLetter() + " ";
+                }
+            }
+
+            //adding player 3's hand if it's not null
+            if(this.player3Hand != null) {
+                output += "player three hand: \n";
+                for (int i = 0; i < 7; i++) {
+                    output += this.player3Hand[i].getLetter() + " ";
+                }
+            }
+
+            //adding player 4's hand if it's not null
+            if(this.player4Hand != null) {
+                output += "player four hand: \n";
+                for (int i = 0; i < 7; i++) {
+                    output += this.player4Hand[i].getLetter() + " ";
                 }
             }
             output += "\n";
